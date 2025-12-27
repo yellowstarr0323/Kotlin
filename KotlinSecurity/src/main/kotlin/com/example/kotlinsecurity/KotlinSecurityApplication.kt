e65b3386374a823b7ca130cd579bc92617e7a648
yellowstarr0323 <ihansaem60@gmail.com>
2025-12-27 23:04:04 +0900

    KotlinSecurity패키지추가

---
+++ KotlinSecurity/src/main/kotlin/com/example/kotlinsecurity/KotlinSecurityApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSecurityApplication>(*args)
+}
